84c3e33c51f50893aa0fd3faba6a768001110415
David Michel <david.michel2809@gmail.com>
2023-01-25 19:02:09 +0100

    feat: modify router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import Router from "./Router";
-import Header from "./pages/Header/Header";
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-    </Router>
+    <Router />
   );
 };
 
--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Footer from "./pages/Footer/Footer";
+import Header from "./pages/Header/Header";
 import NotFound from "./pages/NotFound/NotFound";
 
 const Router = ({ children }) => {
   return (
     <BrowserRouter>
-      {children}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
