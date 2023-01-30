18ac7c8e384c76836b5ff06c81eebaeed7be6580
David Michel <david.michel2809@gmail.com>
2023-01-30 13:46:40 +0100

    doc: add jsdoc types for json data #20

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./types";
 import Router from "./Router";
 
 const App = () => {
---
+++ src/types.js
@@ -0,0 +1,32 @@
+/**
+ * @typedef Host
+ * @type {{
+ * name: string,
+ * picture: string
+ * }}
+ */
+
+/**
+ * @typedef Lodging 
+ * @type {{
+ *  id: string,
+ *  title: string,
+ *  cover: string,
+ *  pictures: string[]
+ *  description: string,
+ *  host: {
+ *  name: string,
+ *  picture: string
+ * },
+ * rating: string,
+ * location: string,
+ * equipments: string[],
+ * tags: string[],
+ * }}
+*/
+
+/**
+ * @typedef Lodgings
+ * @type {Lodging[]}
+*/
+
