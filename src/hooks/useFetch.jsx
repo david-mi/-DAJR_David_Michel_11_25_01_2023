86f1a691e9e7cb79a16f58b3c55a7aeec2523edb
David Michel <david.michel2809@gmail.com>
2023-01-30 14:06:36 +0100

    fix: setting default Isloading state to true in useFetch hook #16

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function getData() {
