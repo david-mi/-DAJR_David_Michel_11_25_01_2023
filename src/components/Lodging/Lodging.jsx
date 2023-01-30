0f0a508c9a0b8c2c1f8901d63920926563301090
David Michel <david.michel2809@gmail.com>
2023-01-30 17:20:53 +0100

    feat: add logic to fetch data, and find correct lodging by id. show loader while fetching and show NotFound component if find method returns null #25

--- src/components/Lodging/Lodging.jsx
+++ src/components/Lodging/Lodging.jsx
@@ -1,11 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
 import styles from "./lodging.module.scss";
 import useFetch from "../../hooks/useFetch";
-import { logementsUrl } from "../../urls";
+import { lodgingsUrl } from "../../urls";
 import NotFound from "../../pages/NotFound/NotFound";
+import Loader from "../Loader/Loader";
+import findLodgingById from "../../helpers/findLodgingById";
 
 const Lodging = () => {
-  return "logding";
+  const { id: lodgingId } = useParams();
+  const { data: lodgings, isLoading, error } = useFetch(lodgingsUrl);
+  const [lodging, setLodging] = useState({});
+
+  useEffect(() => {
+    if (isLoading === false) {
+      console.log(lodgings);
+      const foundLodging = findLodgingById(lodgings, lodgingId);
+      setLodging(foundLodging);
+    }
+  }, [lodgings]);
+
+  if (lodging === null) {
+    return <NotFound />;
+  }
+
+  return (
+    <main className={styles.lodging}>
+      {error && error.message}
+      {isLoading
+        ? <Loader dotColor="black" />
+        : "Carousel"
+      }
+    </main>
+  );
 };
 
 export default Lodging;
