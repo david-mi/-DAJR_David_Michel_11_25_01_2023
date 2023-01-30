826f97d1158652f08bac2c9576ec7ca65b7f7b9a
David Michel <david.michel2809@gmail.com>
2023-01-30 14:32:15 +0100

    feat: refacto Loader component #23

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,14 @@
 import styles from "./loader.module.scss";
 
-const Loader = () => {
+const Loader = ({ dotColor = "white" }) => {
+  const numberOfDots = 12;
+  const dotsJsx = Array(numberOfDots)
+    .fill(null)
+    .map((_, index) => <div key={index} style={{ background: dotColor }}></div>);
+
   return (
     <div className={styles.loader}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      {dotsJsx}
     </div>
   );
 };
