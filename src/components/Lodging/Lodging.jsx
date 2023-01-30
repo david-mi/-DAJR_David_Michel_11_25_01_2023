d89eace0f6e0fd11c1b307f640637780bb722cb2
David Michel <david.michel2809@gmail.com>
2023-01-30 13:58:59 +0100

    refacto: rename all logements by lodging #21

---
+++ src/components/Lodging/Lodging.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import styles from "./lodging.module.scss";
+import useFetch from "../../hooks/useFetch";
+import { logementsUrl } from "../../urls";
+import NotFound from "../../pages/NotFound/NotFound";
+
+const Lodging = () => {
+  return "logding";
+};
+
+export default Lodging;
