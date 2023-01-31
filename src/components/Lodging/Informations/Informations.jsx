c8d3648e586f2469dae08358115f05d5372d0a5d
David Michel <david.michel2809@gmail.com>
2023-01-31 08:56:45 +0100

    feat: add Title Component #29

--- src/components/Lodging/Informations/Informations.jsx
+++ src/components/Lodging/Informations/Informations.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from "./informations.module.scss";
+import { Title } from "./index";
 
 const Informations = ({ lodging }) => {
   const { title, location, host, tags, rating, description, equipment } = lodging;
 
   return (
     <div className={styles.informations}>
-
+      <Title title={title} />
     </div>
   );
 };
