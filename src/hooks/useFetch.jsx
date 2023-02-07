6ee409800935c1d78b09998c59b47e3c5d91d1fd
David Michel <david.michel2809@gmail.com>
2023-02-07 13:53:32 +0100

    doc: add jsdoc for lodging service and useFetch #45

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,5 +1,20 @@
 import { useEffect, useState } from 'react';
 import { lodgingServices } from "../services/lodging";
+import "../types";
+
+/**
+ * Custom Hook to handle fetch calls
+ * - While waiting for service response, {@link isWaitingFetch} is set to true
+ * - If service throws an error, a new {@link error} is set
+ * - If service returns data, setting it to {@link data}
+ * 
+ * @param {string} lodgingId
+ * @returns {{
+ *  data: null | Lodgings | Lodging,
+ *  error: null | Error,
+ *  isWaitingFetch: boolean
+ * }}
+ */
 
 const useFetch = (lodgingId) => {
   const [data, setData] = useState(null);
--- src/services/lodging.js
+++ src/services/lodging.js
@@ -1,10 +1,24 @@
 import { lodgingsUrl } from "../urls";
+import "../types";
 
+/**
+ * Throws an error object, with the given message as parameter when called
+ * 
+ * @param {string} errorMessage 
+ */
 const throwError = (errorMessage) => {
   throw new Error(errorMessage);
 };
 
 export const lodgingServices = {
+
+  /**
+   * Get lodings data from fetch call
+   * 
+   * @throws Throw error if response.ok from fetch call is false
+   * @returns {Promise<Lodgings|Error>}
+   */
+
   async getAllLodgings() {
     const response = await fetch(lodgingsUrl);
     const lodgings = await response.json();
@@ -15,6 +29,14 @@ export const lodgingServices = {
 
     return lodgings;
   },
+
+  /**
+   * - Gets lodings data from fetch call
+   * - Find lodging associated to {@link lodgingId} parameter
+   * 
+   * @throws Throw error if response.ok from fetch call is false
+   * @returns {Promise<Lodging|Error>}
+   */
 
   async getLodgingById(lodgingId) {
     const lodgings = await this.getAllLodgings(lodgingsUrl);
